39f120950b2917e62aca3c982ea20d1baf3f29e2
Rachel Hovell <r.hovell@live.com>
2018-05-15 23:15:10 +0100

    Added scroll to top button
    Added scroll button with functionality

--- java/java.js
+++ java/java.js
@@ -53,8 +53,24 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 
 delayedHeroScroll = setTimeout(function playSlides(){
-  console.log("playSlides called");
+  // console.log("playSlides called");
 plusSlides(n);
 },8000)
 
+}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        document.getElementById("scrollUp").style.display = "block";
+    } else {
+        document.getElementById("scrollUp").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
